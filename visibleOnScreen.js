f2a9d54414ca26e6dd4c8c699fcd9810f8628bcf
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-07 15:23:10 +0200

    Updated 'visibleOnScreen' screen to include detection on wehter the element is above or below.

--- visibleOnScreen.js
+++ visibleOnScreen.js
@@ -1,6 +1,17 @@
 import viewSize from './viewportSize';
 
-export default function visibleOnScreen(node, threshold) {
+export default function visibleOnScreen(node, threshold = 0, mode = 'visible') {
   const rect = node.getBoundingClientRect();
-  return !(rect.bottom - threshold < 0 || rect.top - viewSize().height - threshold >= 0);
+  const above = rect.bottom - threshold < 0;
+  const below = rect.top - viewSize().height + threshold >= 0;
+
+  return mode === 'above' ? above : (mode === 'below' ? below : !above && !below);
+}
+
+export function belowScreen(node, threshold = 0) {
+  return visibleOnScreen(node, threshold, 'below');
+}
+
+export function aboveScreen(node, threshold = 0) {
+  return visibleOnScreen(node, threshold, 'above');
 }
