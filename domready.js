fd22e75871882597cc68c7ef369c7d24ad604464
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-07 10:58:43 +0200

    Simplify domReady script

--- domready.js
+++ domready.js
@@ -1,25 +1,7 @@
 import  on from './on';
 
-let domReady = false;
-let evtsSet = false;
-const handlers = [];
-
-function DOMContentLoaded() {
-  handlers.forEach((cb) => cb());
-  domReady = true;
-}
-
 export default function onDomReady(cb) {
   if(typeof cb !== 'function') { return; }
-
-  domReady = domReady || document.readyState === 'complete';
-  if(domReady) { return cb(); }
-
-  handlers.push(cb);
-
-  if(!evtsSet) {
-    on(document, 'DOMContentLoaded', DOMContentLoaded());
-    on(window, 'load', DOMContentLoaded());
-    evtsSet = true;
-  }
+  if(document.readyState === 'complete') { return cb(); }
+  on(document, 'DOMContentLoaded', cb());
 }
