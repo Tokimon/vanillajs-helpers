3286f76abab6f9fc30b99f4333275a1893379427
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-07 11:19:10 +0200

    Simplified matchesSelector script

--- matchesSelector.js
+++ matchesSelector.js
@@ -1,12 +1,8 @@
 import prefixed from './prefixed';
 
-let _matchMethod;
+const div = document.createElement('div');
+const _matchMethod = div.matches || prefixed('MatchesSelector').filter((method) => !!div[method])[0];
 
 export default function matchesSelector(elm, selector) {
-  if(_matchMethod) { return _matchMethod.call(elm, selector); }
-
-  _matchMethod = elm.matches || prefixed('MatchesSelector').filter((method) => !!elm[method])[0];
-  if(!_matchMethod) { _matchMethod = () => false; }
-
-  return matchesSelector(elm, selector);
+  return _matchMethod.call(elm, selector);
 }
