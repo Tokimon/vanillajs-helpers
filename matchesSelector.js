5cf20fcdd8cac1aa99d25fd0570269e407c11d2a
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-07 10:59:25 +0200

    Simplify 'matchesSelector' script

--- matchesSelector.js
+++ matchesSelector.js
@@ -1,21 +1,12 @@
-import prefixed from '../utils/prefixed';
+import prefixed from './prefixed';
 
 let _matchMethod;
 
 export default function matchesSelector(elm, selector) {
   if(_matchMethod) { return _matchMethod.call(elm, selector); }
 
-  if(typeof elm.matches === 'function') {
-    _matchMethod = elm.matches;
-  } else {
-    prefixed('MatchesSelector').forEach((method) => {
-      if(typeof elm[method] === 'function' && _matchMethod) {
-        _matchMethod = elm[method];
-      }
-    });
-
-    if(!_matchMethod) { _matchMethod = () => false; }
-  }
+  _matchMethod = elm.matches || prefixed('MatchesSelector').filter((method) => !!elm[method])[0];
+  if(!_matchMethod) { _matchMethod = () => false; }
 
   return matchesSelector(elm, selector);
 }
