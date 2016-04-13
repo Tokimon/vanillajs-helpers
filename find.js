76acdbc173ea1767f2113bb716903867813749ab
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-13 16:19:08 +0200

    Move 'findOne' into 'find'

--- find.js
+++ find.js
@@ -1,24 +1,43 @@
 /* eslint no-cond-assign: "off" */
 
-export default function find(selector, context = document, one=false) {
+/**
+ * Uses the correct method to find elements in the DOM from a given selector
+ * @param  {String} selector - CSS query selector
+ * @param  {HTMLElement=document} elm - The HTML Element from where to start the search
+ * @param  {Boolean=false} first - Should the function only return the first found Element or All
+ * @return {Array<HTMLElement>|HTMLElement|NULL} - The found element(s) or null/empty array
+ */
+export default function find(selector, elm = document, first=false) {
   let m, nodes;
 
   // classname(s)
   if(m = /^[.\w-]+$/.exec(selector)) {
-    nodes = context.getElementsByClassName(m[0].replace(/\./g, ' '));
+    nodes = elm.getElementsByClassName(m[0].replace(/\./g, ' '));
 
   // Tag or ID
   } else if(m = /^(#)?([\w-]+)$/.exec(selector)) {
-    nodes = context[`getElement${m[1] ? 'ById' : 'sByTagName'}`](m[2]);
+    nodes = elm[`getElement${m[1] ? 'ById' : 'sByTagName'}`](m[2]);
 
   // name selection
   } else if(m = /^[\[]name=["']?([^'"\]]+)["']?[\]]$/i.exec(selector)) {
-    nodes = context.getElementsByName(m[1]);
+    nodes = elm.getElementsByName(m[1]);
 
   // query selection
   } else {
-    nodes = context[`querySelector${one ? '' : 'All'}`](selector);
+    nodes = elm[`querySelector${first ? '' : 'All'}`](selector);
   }
 
-  return one ? (isNaN(nodes.length) ? nodes : nodes[0]) : [...nodes];
+  if(!nodes) { return first ? null : []; }
+  return first ? (isNaN(nodes.length) ? nodes : nodes[0]) : [...nodes];
+}
+
+/**
+ * Finds the first HTML Element from a given selector
+ * (Shortcut for `find(selector, elm, true)`)
+ * @param  {String} selector - CSS query selector
+ * @param  {HTMLElement=document} elm - The HTML Element from where to start the search
+ * @return {HTMLElement|NULL} - The found element or null
+ */
+export function findOne(selector, elm = document) {
+  return find(selector, elm, true);
 }
--- findOne.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import find from './find';
-
-export function findOne(selector, context = document) {
-  return find(selector, context, true);
-}
