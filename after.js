f148ab170bf9a3e50e812b6e59e026feac0e0ddb
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-13 16:21:05 +0200

    Enable 'before' and 'after' script to also take actual HTML elements and not just HTML

--- after.js
+++ after.js
@@ -1,7 +1,17 @@
-export default function after(context, html) {
-  if(!context.parentNode) {
-    return console.warn('You can only insert content after elements that are inside the DOM tree');
+import { isString } from './objectType';
+
+/**
+ * Inserts HTML Element or plain HTML after a given HTML Element
+ * @param  {HTMLElement} elm - The HTML Element in question
+ * @param  {String|HTMLElement} insertElm - HTML Element or String to insert after the {elm}
+ */
+export default function after(elm, insertElm) {
+  const parent = elm.parentNode;
+
+  if(!parent) {
+    return console.error('You can only insert content after elements that are inside the DOM tree');
   }
 
-  context.insertAdjacentHTML('afterend', html);
+  if(insertElm.nodeType) { parent.insertBefore(insertElm, elm.nextSibling); }
+  else if(isString(insertElm)) { elm.insertAdjacentHTML('afterend', insertElm); }
 }
--- before.js
+++ before.js
@@ -1,7 +1,17 @@
-export default function before(context, html) {
-  if(!context.parentNode) {
-    return console.warn('You can only insert content before elements that are inside the DOM tree');
+import { isString } from './objectType';
+
+/**
+ * Inserts HTML Element or plain HTML before a given HTML Element
+ * @param  {HTMLElement} elm - The HTML Element in question
+ * @param  {String|HTMLElement} insertElm - HTML Element or String to insert before the {elm}
+ */
+export default function before(elm, insertElm) {
+  const parent = elm.parentNode;
+
+  if(!parent) {
+    return console.error('You can only insert content before elements that are inside the DOM tree');
   }
 
-  context.insertAdjacentHTML('beforebegin', html);
+  if(insertElm.nodeType) { parent.insertBefore(insertElm, elm); }
+  else if(isString(insertElm)) { elm.insertAdjacentHTML('beforebegin', insertElm); }
 }
