3cc56d96392f82c3bdea3e5a2a952e71005cd181
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-13 16:15:49 +0200

    Rename 'toDom' to 'htmlToDom'

--- htmlToDom.js
+++ htmlToDom.js
@@ -1,6 +1,11 @@
 import children from './children';
 import { isString } from './objectType';
 
+/**
+ * Convert HTML into DOM node(s)
+ * @param  {String} html - HTML string to transform into nodes
+ * @return {Array<HTMLElement>} - List of converted HTML Elements
+ */
 export default function htmlToDom(html) {
   if(!isString(html)) { return html; }
   const div = document.createElement('div');
