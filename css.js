8451033645f86ba914312307a98345846126ef2a
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-13 16:16:27 +0200

    Update 'css' script to also be able to set styles

--- css.js
+++ css.js
@@ -1,5 +1,21 @@
-// TODO: Add support for setting styles
+/**
+ * Get current styling of a HTML element and optionally set given style first
+ * @param  {HTMLElement} elm - HTML Element to get the style from
+ * @param  {Object} style - [optional] Styling to set on the elment
+ * @return {Object} - Current styling on the element
+ */
+export default function css(elm, style) {
+  // If styles are defined, then add them to the elments inline style
+  if(style) {
+    // Go through each style
+    Object.keys(style).forEach((key) => {
+      // camelCase the key
+      const camelKey = key.replace(/-[a-z]/gi, (m) => m[1].toUpperCase());
+      // Set the style
+      elm.style[camelKey] = style[key];
+    });
+  }
 
-export default function css(node) {
-  return window.getComputedStyle(node);
+  // return current style
+  return window.getComputedStyle(elm);
 }
