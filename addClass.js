c93d9c68580d2d160e2a02d66b41a1e1ab235f10
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-18 17:50:26 +0200

    Combined class handler methods into on file: 'className'

--- addClass.js
+++
@@ -1,8 +0,0 @@
-/**
- * Adds one or several class names to a HTML Element
- * @param {HTMLElement} elm - HTML ELement in question
- * @param {Array<string>} ...classNames - Class names to add (each as an arguments)
- */
-export default function addClass(elm, ...classNames) {
-  return elm.classList.add(...classNames);
-}
--- removeClass.js
+++
@@ -1,8 +0,0 @@
-/**
- * Remove one or several class names from a HTML Element
- * @param {HTMLElement} elm - HTML ELement in question
- * @param {Array<string>} ...classNames - Class names to remove (arguments)
- */
-export default function removeClass(elm, ...classNames) {
-  return elm.classList.remove(...classNames);
-}
