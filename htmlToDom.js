48683df9f025621ffcebd3b6da1b8b996e65c403
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-18 17:54:14 +0200

    Switched from 'createElement' to 'DOMParser' method to render html.

--- htmlToDom.js
+++ htmlToDom.js
@@ -1,14 +1,11 @@
-import children from './children';
 import { isString } from './objectType';
 
 /**
  * Convert HTML into DOM node(s)
  * @param  {String} html - HTML string to transform into nodes
- * @return {Array<HTMLElement>} - List of converted HTML Elements
+ * @return {Array<HTMLElement>} - HTML Elements that the HTML represented
  */
 export default function htmlToDom(html) {
   if(!isString(html)) { return html; }
-  const div = document.createElement('div');
-  div.innerHTML = html;
-  return children(div);
+  return (new DOMParser()).parseFromString(html, 'text/html');
 }
