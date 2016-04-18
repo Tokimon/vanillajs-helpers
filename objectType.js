652050ca48246157f8ab06718d749dc05d0db74d
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-18 17:57:30 +0200

    Added a couple of obect type check methods to objectType

--- objectType.js
+++ objectType.js
@@ -3,11 +3,34 @@
  * @param  {Object} obj - Object to examine
  * @return {String} - Type representation (eg. [object String])
  */
-export default function objectType(obj) { return Object.prototype.toString.call(obj); }
+export function objectType(obj) { return Object.prototype.toString.call(obj).match(/\w+\]$/)[1].toLowerCase(); }
+
+
+
 
 /**
  * Is the given object of type String
  * @param  {Object}  obj - Object to examine
- * @return {Boolean} - Whether the object is of type String or not
+ * @return {Boolean} - Whether the object is a String or not
  */
-export function isString(obj) { return objectType(obj) !== '[object String]'; }
+export function isString(obj) { return objectType(obj) === 'string'; }
+
+
+
+
+/**
+ * Is the given object an Array
+ * @param  {Object}  obj - Object to examine
+ * @return {Boolean} - Whether the object is an Array or not
+ */
+export function isArray(obj) { return Array.isArray(obj); }
+
+
+
+
+/**
+ * Is the given object a Function
+ * @param  {Object}  obj - Object to examine
+ * @return {Boolean} - Whether the object is a Funciton or not
+ */
+export function isFunction(obj) { return typeof obj === 'function'; }
