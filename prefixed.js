12dd2f42a6e1c0093f9ce073f6e0d41017658cb7
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-19 16:22:42 +0200

    prefixed: forced Pascal case on the string given + updated comments

--- prefixed.js
+++ prefixed.js
@@ -1,8 +1,11 @@
+import pascalCase from './pascalCase';
+
 /**
- * Add vendor prefixes to the string
- * @param  {String} str - String to add the vendor prefixes to
+ * Add vendor prefixes to a string
+ * @param  {String} str - String to add vendor prefixes to
  * @return {Array<String>} Array of the various vendor prefixed versions of the string
  */
 export default function prefixed(str) {
-  return [ `webkit${str}`, `moz${str}`, `ms${str}`, `o${str}` ];
+  str = pascalCase(str);
+  return [`webkit${str}`, `moz${str}`, `ms${str}`, `o${str}`];
 }
