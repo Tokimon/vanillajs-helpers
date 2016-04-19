9c66284306cfddb74cc902bf9fe1fc3e4206a8bd
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-19 16:08:22 +0200

    css: Added possibility to find styling of a pseudoelement

--- css.js
+++ css.js
@@ -1,21 +1,28 @@
-import { camelCase } from './wordCasing';
+import { isString } from './objectType';
 
 /**
  * Get current styling of a HTML element and optionally set given style first
  * @param  {HTMLElement} elm - HTML Element to get the style from
+ * @param  {Object} [pseudo] - Pseudo element to get the styling from
  * @param  {Object} [style] - Styling to set on the element
  * @return {Object} - Current styling on the element
  */
-export default function css(elm, style) {
+export default function css(elm, pseudo, style) {
+  if(pseudo && !isString(pseudo)) {
+    style = pseudo;
+    pseudo = null;
+  }
+
   // If styles are defined, then add them to the elments inline style
   if(style) {
     // Go through each style
     Object.keys(style).forEach((key) => {
       // Set the style
-      elm.style[camelCase(key, '-')] = style[key];
+      const val = style[key].match(/(.*)(?:\s+([!]important))?$/);
+      elm.style.setProperty(key, val[1], val[2]);
     });
   }
 
-  // return current style
-  return window.getComputedStyle(elm);
+  // TODO: Find a way to return null when pseudo is defined but there is no pseudo element for the element
+  return window.getComputedStyle(elm, pseudo);
 }
