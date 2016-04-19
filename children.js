c0520f270030d5704cc2824faf97d9cbccca4803
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-19 16:07:33 +0200

    children: Made document.body default value of elm

--- children.js
+++ children.js
@@ -3,8 +3,7 @@
  * @param  {HTMLElement} elm - HTML Element to find children from
  * @return {Array<HTMLElement>} - List of found child HTML Elements
  */
-export function children(elm) {
-  if(!elm) { return []; }
+export default function children(elm = document.body) {
   if(elm.children) { return [...elm.children]; }
   return [...elm.childNodes].filter((child) => child.nodeType === 1);
 }
