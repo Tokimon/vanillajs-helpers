dfc098ffd43ffdffc8e5ce4513b4d72158dfce7d
Toke Voltelen <tokevoltelen@gmail.com>
2016-04-28 18:16:42 +0200

    Add randomRGBColor script

---
+++ randomRGBColor.js
@@ -0,0 +1,7 @@
+function c() { return Math.round(Math.random() * 255); }
+
+/**
+ * Generate a random RGB color
+ * @return {Array} - Three random numbers from 0-255 as an array ([r, g, b])
+ */
+export default function randomRGBColor() { return [c(), c(), c()]; }
