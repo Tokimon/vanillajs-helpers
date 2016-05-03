e9ca68c7762ab719a7423d594162bde7a3b5f9f3
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-03 04:31:49 +0200

    Append: Changed check to just isHTMLElement from isDOMElement

--- append.js
+++ append.js
@@ -1,5 +1,5 @@
 import isString from './isString';
-import isDOMElement from './isDOMElement';
+import isHTMLElement from './isHTMLElement';
 
 /**
  * Append HTML Element or plain HTML to the end of a given HTML Element
@@ -7,7 +7,7 @@ import isDOMElement from './isDOMElement';
  * @param  {String|HTMLElement} insertElm - HTML Element or String to append to the {elm}
  */
 export default function append(elm, insertElm) {
-  if(!isDOMElement(elm)) { return; }
-  if(isString(insertElm)) { elm.insertAdjacentHTML('beforeend', insertElm); }
-  else if(insertElm.nodeType) { elm.appendChild(insertElm); }
+  if(!isHTMLElement(elm) || !insertElm) { return; }
+  if(insertElm.nodeType) { elm.appendChild(insertElm); }
+  else if(isString(insertElm)) { elm.insertAdjacentHTML('beforeend', insertElm); }
 }
