7444b6d50a017226ecdc7232e6c92af05d729604
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-03 03:34:50 +0200

    isDOMChild: Add missing argument

--- isDOMChild.js
+++ isDOMChild.js
@@ -1,6 +1,6 @@
 import isDOMElement from './isDOMElement';
 import isDOMRoot from './isDOMRoot';
 
-export default function isDOMChild() {
-  return isDOMElement(elm) || !isDOMRoot(elm)
+export default function isDOMChild(elm) {
+  return isDOMElement(elm) && !isDOMRoot(elm);
 }
