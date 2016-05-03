693a1d5004d72b5dc486702a6a33097239394bef
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-03 04:34:16 +0200

    Prepend: Corrected argument checks and reversed arguments in 'inserBefore' call

--- prepend.js
+++ prepend.js
@@ -1,5 +1,5 @@
 import isString from './isString';
-import isDOMElement from './isDOMElement';
+import isHTMLElement from './isHTMLElement';
 
 /**
  * Prepend HTML Element or plain HTML into a given HTML Element
@@ -7,7 +7,7 @@ import isDOMElement from './isDOMElement';
  * @param  {string|HTMLElement} insertElm - HTML Element or String to prepend to the {elm}
  */
 export default function prepend(elm, insertElm) {
-  if(!isDOMElement(elm)) { return; }
+  if(!isHTMLElement(elm) || !insertElm) { return; }
   if(isString(insertElm)) { elm.insertAdjacentHTML('afterbegin', insertElm); }
-  else if(insertElm.nodeType) { elm.insertBefore(elm.firstChild, insertElm); }
+  else if(insertElm.nodeType) { elm.insertBefore(insertElm, elm.firstChild); }
 }
