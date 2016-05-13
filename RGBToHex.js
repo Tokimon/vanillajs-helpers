99416e621a0dfa3075082eb8dd872015874b8736
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-13 13:38:20 +0200

    RGBToHex: Correct functino name casing

--- RGBToHex.js
+++ RGBToHex.js
@@ -7,7 +7,7 @@ function hex(num = 0) { return (0 + num.toString(16)).substr(-2); }
  * @param  {Number}       b - Blue color saturation (0-255)
  * @return {String}         - Hex color representation ('#2fd466')
  */
-export default function rgbToHex(r, g, b) {
+export default function RGBToHex(r, g, b) {
   if(Array.isArray(r)) { [r, g, b] = r; }
   return `#${hex(r)}${hex(g)}${hex(b)}`;
 }
