aacbffa3674602a2ae5787a3a45a73b6c3a89ab0
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-18 10:38:42 +0200

    findByName: Create

---
+++ findByName.js
@@ -0,0 +1,17 @@
+import isDOMElement from './isDOMElement';
+import isDOMDocument from './isDOMDocument';
+import isString from './isString';
+import isArray from './isArray';
+
+export default function findByName(names) {
+  // Is it is a string split by comma (convert to Array)
+  if(isString(names)) { names = names.split(/[\s,]+/); }
+
+  // 'elm' has to be either a HTMLElement or 'document'
+  // and clasNames has to be an Array
+  if(!isArray(names)) { return []; }
+
+  return names.reduce((arr, name) => {
+    return arr.concat(name ? Array.from(document.getElementsByName(name)) : []);
+  }, []);
+}
