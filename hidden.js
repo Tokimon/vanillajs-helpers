2a8ccefdd42bd56841acd210a17e890de836119d
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-18 10:32:18 +0200

    hidden: Improved the check a bit

--- hidden.js
+++ hidden.js
@@ -1,4 +1,4 @@
-import isChildElement from './isChildElement';
+import inDOM from './inDOM';
 
 /**
  * Test if a given HTML element is technically hidden ('display:none' or 'visibility: hidden').
@@ -6,7 +6,7 @@ import isChildElement from './isChildElement';
  * @return {Boolean} - Is the element technically hidden or not
  */
 export default function hidden(elm) {
-  if(!isChildElement(elm)) { return true; }
-  if(!elm.offsetHeight && !elm.offsetWidth) { return true; }
-  return getComputedStyle(elm).visibility === 'hidden';
+  return !inDOM(elm)
+          || !(elm.offsetHeight || elm.offsetWidth)
+          || getComputedStyle(elm).visibility === 'hidden';
 }
