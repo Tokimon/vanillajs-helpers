ce5e8b008598d53766c4de66bc9c3b024bd34778
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-18 10:30:04 +0200

    pascalCase: Utilize camelCase script

--- pascalCase.js
+++ pascalCase.js
@@ -1,9 +1,11 @@
+import camelCase from './camelCase';
+
 /**
  * Transform a phrase into a PascalCased word (eg. 'pascal case' -> 'PascalCase')
  * @param {String} str - String to transform
  * @param {String=" "} separator - Separator between words
  * @return {String} - Transformed string
  */
-export default function pascalCase(str, separator = ' ') {
-  return str.toLowerCase().replace(new RegExp(`(?:^|${separator}+)([a-z])`, 'gi'), (match, char) => char.toUpperCase());
+export default function pascalCase(str) {
+  return camelCase({ upper: true })(str);
 }
