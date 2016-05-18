7bd95be955d2a62f0cd82787fc67352186fe58f4
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-18 10:33:02 +0200

    visible: Change to be based on invisible

--- visible.js
+++ visible.js
@@ -1,14 +1,10 @@
+import invisible from './invisible';
+
 /**
- * Test if a given HTML element is visibile (as in viewable) for the user.
- * - Important distinction from 'hidden' is that this test for collapsed and opaque
- * elements as well, as those wouldn't be visible for the user.
+ * Test if a given HTML element is visibile.
  * @param  {HTMLElement} elm - HTML element to test
  * @return {Boolean} - Is the element visible for the user
  */
 export default function visible(elm) {
-  // 'display: none' and collapsed elements
-  if(!elm.offsetHeight || !elm.offsetWidth) { return false; }
-  const style = getComputedStyle(elm);
-  // 'visibility: hidden' and 'opacity: 0' elements
-  return style.visibility !== 'hidden' && !!Number(style.opacity);
+  return !invisible(elm);
 }
