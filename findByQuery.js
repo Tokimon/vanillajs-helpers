22a82b8c64726119ac789e33a64f05629960a7b6
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-24 18:31:06 +0200

    findByQuery: Improve script execution timing a bit and silence errors

--- findByQuery.js
+++ findByQuery.js
@@ -1,5 +1,4 @@
-import isDOMElement from './isDOMElement';
-import isDOMDocument from './isDOMDocument';
+import isBoolean from './isBoolean';
 import isString from './isString';
 import isArray from './isArray';
 
@@ -7,15 +6,15 @@ export default function findByQuery(queries, elm = document, first = false) {
   // Correct variables if 'elm' is omitted but 'first' isn't
   if(isBoolean(elm)) { [elm, first] = [document, elm]; }
 
-  // Is it is a string split by comma (convert to Array)
-  if(isArray(queries)) { queries = queries.join(','); }
-
-  // 'elm' has to be either a HTMLElement or 'document'
-  // and queries has to be an Array
-  if(!isString(queries)) { return first ? null : []; }
+  // If we got an array, filter out bad queries
+  if(isArray(queries)) {
+    queries = queries.filter((query) => isString(query)).join(',');
+  }
 
   // 'elm' must be an object with the 'querySelector' implementation
-  if(!(elm && elm.querySelector)) { elm = document; }
+  if(!elm) { elm = document; }
 
-  return first ? elm.querySelector(query) : Array.from(elm.querySelectorAll(query));
+  try {
+    return first ? elm.querySelector(queries) : Array.from(elm.querySelectorAll(queries));
+  } catch(ex) { return first ? null : []; }
 }
