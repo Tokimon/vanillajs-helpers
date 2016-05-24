7eacfbb335c67e9369a42def35c61e91ebe2e436
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-24 18:29:15 +0200

    findById: Optimize script by only performing loop when necessary

--- findById.js
+++ findById.js
@@ -5,12 +5,12 @@ export default function findById(ids) {
   // Is it is a string split by comma and/or space (convert to Array)
   if(isString(ids)) { ids = ids.split(/[\s,]+/); }
 
-  // 'ids' has to be an Array
+  // 'ids' has to be an Array at this point
   if(!isArray(ids)) { return null; }
 
-  // Search elements from each ID and filter out results that returned NULL
-  const nodes = ids.map((id) => isString(id) ? document.getElementById(id) : null).filter((elm) => !!elm);
+  // If we have only one query, just find and return that
+  if(ids.length < 2) { return document.getElementById(ids[0]); }
 
-  // Return Array if ids had more than one entry otherwise just the first result
-  return ids.length > 1 ? nodes : nodes[0] || null;
+  // Search elements from each ID and filter out results that returned NULL
+  return ids.map((id) => isString(id) ? document.getElementById(id) : null).filter((elm) => !!elm);
 }
