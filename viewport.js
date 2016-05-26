0bfce6a48d00141a87ec5d9eed8a09eb9b4899c9
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-26 13:41:50 +0200

    viewport: Make viewport a function to consider iframes

--- viewport.js
+++ viewport.js
@@ -1,3 +1,10 @@
-// Expose the determined viewport element
-const viewport = document.scrollingElement || (document.compatMode === 'BackCompat' ? document.body : document.documentElement);
-export default viewport;
+import isDOMDocument from 'isDOMDocument';
+
+export default function viewport(doc = document) {
+  if(!isDOMDocument(doc)) {
+    doc = doc.ownerDocument || elm.document;
+    if(!doc) { return null; }
+  }
+
+  return doc.scrollingElement || (doc.compatMode === 'BackCompat' ? doc.body : doc.documentElement)
+}
