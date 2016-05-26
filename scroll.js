2878330894a28fd0a7ad3bf5e0f21f9cea3265a3
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-26 13:40:07 +0200

    scroll: Improve element check + include scroll position object to set the current scrolling position

--- scroll.js
+++ scroll.js
@@ -1,21 +1,47 @@
 import viewport from './viewport';
+import size from './size';
+import isObject from './isObject';
+import isWindow from './isWindow';
+import isDOMElement from './isDOMElement';
+import inDOM from './inDOM';
 
 /**
  * Find the current scroll position of a HTML Element
  * @param  {HTMLElement|window} [elm = viewport] - The HTML element to find the scrolling position from
  * @return {Object} - The current scroll information
  */
-export default function scroll(elm = viewport) {
-  // Take the viewport if window is the element
-  if(elm === window) { elm = viewport; }
+export default function scroll(elm = window, scrollPos = null) {
+  let isWin = isWindow(elm);
 
-  const width = elm.offsetParent ? elm.offsetWidth : window.innerWidth;
-  const height = elm.offsetParent ? elm.offsetHeight : window.innerHeight;
+  if(!isWin) {
+    if(isObject(elm)) { [elm, scrollPos] = [window, elm]; }
+
+    if(isDOMElement(elm, 'html', 'body')) { elm = elm.ownerDocument; }
+    if(typeof elm.defaultView !== 'undfined') { elm = elm.defaultView; }
+
+    isWin = isWindow(elm);
+
+    if(!(isDOMElement(elm) && inDOM(elm)) || isWin) { return null; }
+  }
+
+  const view = isWin ? viewport(elm) : elm;
+
+  if(scrollPos) {
+    const { byX, byY, x, y } = scrollPos;
+
+    if(!isNaN(x)) { view.scrollTop = x; }
+    else if(!isNaN(byX)) { view.scrollTop += byX; }
+
+    if(!isNaN(y)) { view.scrollTop = y; }
+    else if(!isNaN(byY)) { view.scrollTop += byY; }
+  }
+
+  const s = size(elm);
 
   return {
-    top: elm.scrollTop,
-    maxTop: elm.scrollHeight - height,
-    left: elm.scrollLeft,
-    leftMax: elm.scrollWidth - width
+    x: view.scrollLeft,
+    xMax: s.contentWidth - s.innerWidth,
+    y: view.scrollTop,
+    yMax: s.contentHeight - s.innerHeight
   };
 }
