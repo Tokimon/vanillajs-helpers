d0ec8eb14e29e2c99b052d3efc72e4e3849b345d
Toke Voltelen <tokevoltelen@gmail.com>
2016-05-31 17:39:15 +0200

    chunkWord: Rename to 'chunkString' and improve algorithm

---
+++ chunkString.js
@@ -0,0 +1,9 @@
+/**
+ * Split a String up into smaller strings of a give size (eg. 'ABCDEF' -> [AB,CD,EF])
+ * @param  {String} [str=''] String to split into smaller parts
+ * @param  {Number} [size=2] The length of each chunk
+ * @return {Array} String split up in smaller string of the given size
+ */
+export default function chunkString(str = '', size = 2) {
+  return (''+ str).match(new RegExp(`.{1,${size}}(?=\\B|$)`, 'g')) || [];
+}
--- chunkWord.js
+++
@@ -1,3 +0,0 @@
-export default function chunkWord(str = '', size = 2) {
-  return str.match(new RegExp(`.{1,${size}}(?=\\B|$)`, 'g'));
-}
