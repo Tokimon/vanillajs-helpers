f6a90f493bf9d31738dc09e17aa1578f0a18182d
Toke Voltelen <tokevoltelen@gmail.com>
2016-06-02 01:59:27 +0200

    on, off: Improved arguments check

--- off.js
+++ off.js
@@ -1,13 +1,15 @@
 import words from './eachWord';
+import isFunction from './isFunction';
 
 /**
  * Unbinds events for the given element.
  * @param  {HTMLElement} elm - HTML Element to unbind the event from
  * @param  {String} eventNames - Space seperated string of event names to unbind the handler from
  * @param  {Function} handler - Handler to unbind from the event(s)
- * @return {Number} - The number of event mentioned
+ * @return {HTMLElement} - The 'elm' or NULL
  */
-export function off(elm, eventNames, handler) {
+export default function off(elm, eventNames, handler) {
+  if(!elm || !isFunction(elm.addEventListener) || !isFunction(handler)) { return null; }
   words(eventNames, (name) => elm.removeEventListener(name, handler, true));
-  return handler;
+  return elm;
 }
--- on.js
+++ on.js
@@ -1,13 +1,15 @@
 import words from './eachWord';
+import isFunction from './isFunction';
 
 /**
  * Bind event handler for one or more event names (seperated by space).
  * @param  {HTMLElement} elm - HTML Element to bind the event to
  * @param  {String} eventNames - Space seperated string of event names to bind the handler to
  * @param  {Function} handler - Handler to bind to the event(s)
- * @return {Function} - Bound handler
+ * @return {HTMLElement} - The 'elm' or NULL
  */
 export default function on(elm, eventNames, handler) {
+  if(!elm || !isFunction(elm.addEventListener) || !isFunction(handler)) { return null; }
   words(eventNames, (name) => elm.addEventListener(name, handler, true));
-  return handler;
+  return elm;
 }
