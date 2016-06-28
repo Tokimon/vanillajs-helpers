fc1e78ffdf023cfaf7b90bb8081955204d71e9c5
Toke Voltelen <tokevoltelen@gmail.com>
2016-06-28 11:09:58 +0200

    domReady: Create a 'domReadyBuilder' and imporve algorithm

--- domready.js
+++ domready.js
@@ -1,11 +1,30 @@
+import _once from './once';
+import isFunction from './isFunction';
+
+
+
+
+/**
+ * Build a method that executes a given function once the document has finished loading
+ * @param  {Function} once - The 'once' (single trigger event) method to use
+ * @return {Function} - The 'domready' method using the given once method
+ */
+export function domReadyBuilder(once = _once) {
+  if(!isFunction(once)) { return null; }
+
+  return (cb) => {
+    if(!isFunction(cb)) { return; }
+    if(document.readyState === 'complete') { return cb(); }
+    once(document, 'DOMContentLoaded', () => cb());
+  }
+}
+
+
+
+
 /**
  * Execute a given function once the document has finished loading
  * @param  {Function} cb - Function to execute once the document has finished loading
  */
-export default function domReady(once) {
-  return (cb) => {
-    if(typeof cb !== 'function') { return; }
-    if(document.readyState === 'complete') { return cb(); }
-    once(document, 'DOMContentLoaded', () => cb());
-  };
-}
+const domReady = domReadyBuilder();
+export default domReady;
