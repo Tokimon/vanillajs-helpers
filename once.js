04be723a9c8f3a40abedec5c6de793957a50ae33
Toke Voltelen <tokevoltelen@gmail.com>
2016-06-28 11:10:53 +0200

    once: Create 'onceBuilder' and improve algorithm

--- once.js
+++ once.js
@@ -1,3 +1,24 @@
+import _on from './on';
+import _off from './off';
+import isFunction from './isFunction';
+
+
+
+
+export function onceBuilder(on = _on, off = _off) {
+  if(!isFunction(on) || !isFunction(off)) { return null; }
+
+  return (elm, eventNames, handler) => {
+    return on(elm, eventNames, function _one(e) {
+      off(elm, e.type, _one);
+      return handler.call(this, e);
+    });
+  }
+}
+
+
+
+
 /**
  * Bind a single fire event handler for one or more event names (seperated by space).
  * Each event bound to will only trigger the handler once.
@@ -6,11 +27,5 @@
  * @param  {Function} handler - Handler to bind to the event(s)
  * @return {Function} - Bound handler
  */
-export function once(on, off) {
-  return (elm, eventNames, handler) => {
-    return on(elm, eventNames, function _one(e) {
-      off(elm, e.type, _one);
-      return handler.call(this, e);
-    });
-  };
-}
+const once = onceBuilder();
+export default once;
