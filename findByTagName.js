f76021df9481d5d614332afc63bd8dc87258ce5e
Toke Voltelen <tokevoltelen@gmail.com>
2016-08-23 14:33:41 +0200

    findByTagName: Optimize algorithm

--- findByTagName.js
+++ findByTagName.js
@@ -5,18 +5,17 @@ export default function findByTagName(tags, elm) {
   // Is it is a string split by comma (convert to Array)
   if(isString(tags)) { tags = tags.split(/[\s,]+/); }
 
-  // 'elm' has to be either a HTMLElement or 'document'
-  // and clasNames has to be an Array
+  // Tag names has to be an Array
   if(!isArray(tags)) { return []; }
 
   // 'elm' must be an object with the 'getElementsByTagName' implementation
   if(!elm || !elm.getElementsByTagName) { elm = document; }
 
   try {
-    if(classNames.length < 2) { return Array.from(elm.getElementsByTagName(tags[0])); }
+    if(tags.length < 2) { return Array.from(elm.getElementsByTagName(tags[0])); }
 
     return tags.reduce((arr, tag) => {
-      return !isString(tag) ? arr : arr.concat(Array.from(elm.getElementsByName(tag)));
+      return !isString(tag) ? arr : arr.concat(Array.from(elm.getElementsByTagName(tag)));
     }, []);
   } catch(ex) { return []; }
 }
