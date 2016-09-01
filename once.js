038e8b5b605f3ea70f99b9976a912d092943e216
Toke Voltelen <tokevoltelen@gmail.com>
2016-09-01 18:07:45 +0200

    once: Improve script
    Improvements:
    - Updated descriptions
    - Made the event binder return the bound handler so it can be removed again

--- once.js
+++ once.js
@@ -4,15 +4,28 @@ import isFunction from './isFunction';
 
 
 
-
-export function onceBuilder(on = _on, off = _off) {
-  if(!isFunction(on) || !isFunction(off)) { return null; }
+/**
+ * Build an event binder that will bind event handlers that are triggered only once
+ *
+ * @param  {Function} on - The method to use bind event handlers
+ * @param  {Function} off - The method to use to remove event handlers
+ * @return {Function} - The single fire event binder function
+ */
+export function onceBuilder(on, off) {
+  if(!isFunction(on)) { on = _on; }
+  if(!isFunction(off)) { off = _off; }
 
   return (elm, eventNames, handler) => {
-    return on(elm, eventNames, function _one(e) {
+    if(!isFunction(handler)) { return null; }
+
+    const _one = function(e) {
       off(elm, e.type, _one);
       return handler.call(this, e);
-    });
+    }
+
+    on(elm, eventNames, _one);
+
+    return _one;
   }
 }
 
@@ -21,11 +34,11 @@ export function onceBuilder(on = _on, off = _off) {
 
 /**
  * Bind a single fire event handler for one or more event names (seperated by space).
- * Each event bound to will only trigger the handler once.
+ *
  * @param  {HTMLElement} elm - HTML Element to unbind the event from
  * @param  {String} eventNames - Space seperated string of event names to bind the handler to
  * @param  {Function} handler - Handler to bind to the event(s)
- * @return {Function} - Bound handler
+ * @return {Function} - The single fire event handler (so it may be removed again)
  */
 const once = onceBuilder();
 export default once;
