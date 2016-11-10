4ddae915af77d1378e025bab4a3b3ef04185db6c
Toke Voltelen <tokevoltelen@gmail.com>
2016-11-10 18:54:59 +0100

    uniqueArray: Add check for non Array items

--- uniqueArray.js
+++ uniqueArray.js
@@ -1,8 +1,11 @@
+import isArray from './isArray';
+
 /**
  * Filter out any duplicate values from an array
  * @param  {Array}  [arr=[]] - Array that needs unique values
  * @return {[type]} - The given array with only unique values
  */
-export default function uniqueArray(arr = []) {
+export default function uniqueArray(arr) {
+  if(!isArray(arr)) { return arr; }
   return arr.filter((item, i, a) => a.indexOf(item) === i);
 }
