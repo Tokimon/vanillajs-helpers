dc277b74cd041906c4b66eb812a52cfaa2f40670
Tokimon <tokevoltelen@gmail.com>
2016-11-12 03:00:08 +0100

    Add promisefy

---
+++ promisefy.js
@@ -0,0 +1,11 @@
+/**
+ * Converts a callback based action into returning a Promise instead.
+ *
+ * @param  {Function} fn - The function you want to return a promise instead of using callback
+ * @return {Function} - A version of the given function that now retuns a promise once executed.
+ */
+export default function promisefy(fn) {
+  return (...args) => new Promise((resolve, reject) => {
+    fn(...args, (err, data) => err ? reject(err) : resolve(data));
+  });
+}
