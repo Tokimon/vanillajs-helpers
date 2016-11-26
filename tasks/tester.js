6e36d2c57bb0e675ae7fc64ae68fea0699f454c2
Toke Voltelen <tokevoltelen@gmail.com>
2016-11-26 01:00:21 +0100

    Taks Tester: Make coverage optional via the `--coverage` CLI option

--- tasks/tester.js
+++ tasks/tester.js
@@ -11,6 +11,11 @@ const args = yargs
     describe: 'Which tests to run',
     type: 'array'
   })
+  .option('coverage', {
+    alias: 'c',
+    describe: 'Add coverage report',
+    type: 'boolean'
+  })
   .help()
   .alias('help', 'h')
   .argv;
@@ -19,10 +24,15 @@ const tests = args.test && args.test.length
   ? (args.test.length === 1 ? args.test[0] : `@(${args.test.join('|')})`)
   : '*';
 
-require('babel-register')({
-  presets: ['node5'],
-  plugins: [['istanbul', { include: [`${tests}.js`] }]]
-});
+const babelConfig = {
+  presets: ['node5']
+};
+
+if(args.coverage) {
+  babelConfig.plugins = [['istanbul', { include: [`${tests}.js`] }]];
+}
+
+require('babel-register')(babelConfig);
 
 const mochaConfig = {
   ui: 'bdd',
@@ -37,16 +47,18 @@ glob(nPath.resolve(`test/${tests}.spec.js`))
     const test = new Mocha(mochaConfig);
     test.files = files;
     test.run((errors) => {
-      const reporters = ['text', 'text-summary', 'lcov'];
-      const opts = { dir: './coverage' };
-      const collector = new Istanbul.Collector();
-      collector.add(global.__coverage__ || {});
+      if(args.coverage) {
+        const reporters = ['text', 'text-summary', 'lcov'];
+        const opts = { dir: './coverage' };
+        const collector = new Istanbul.Collector();
+        collector.add(global.__coverage__ || {});
 
-      reporters.forEach((reporter) => {
-        Istanbul.Report
-          .create(reporter, opts)
-          .writeReport(collector, true);
-      });
+        reporters.forEach((reporter) => {
+          Istanbul.Report
+            .create(reporter, opts)
+            .writeReport(collector, true);
+        });
+      }
 
       // exit with non-zero status if there were failures
       process.on('exit', () => process.exit(errors));
