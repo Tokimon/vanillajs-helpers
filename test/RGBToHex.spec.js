5268b9ddc951e6a463983f661510c35e4a80e3ee
Toke Voltelen <tokevoltelen@gmail.com>
2017-08-14 15:21:06 +0200

    RGBToHex: Update to include Alpha channel

--- test/RGBToHex.spec.js
+++ test/RGBToHex.spec.js
@@ -11,4 +11,8 @@ describe('"RGBToHex"', () => {
   it('Should generate a hex color from a RGB entries', () => {
     expect(RGBToHex(0, 0, 255)).to.equal('#0000ff');
   });
+
+  it('Should add the alpha channel if present', () => {
+    expect(RGBToHex(0, 255, 0, 0.5)).to.equal('#00ff0080');
+  });
 });
