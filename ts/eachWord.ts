f31ccb6e714cd9bc641d7b46431ff8a82ea0ad02
Toke Voltelen <tokevoltelen@gmail.com>
2017-11-08 01:36:53 +0100

    eachWord: Correct name to indexLoop

--- ts/eachWord.ts
+++ ts/eachWord.ts
@@ -1,6 +1,6 @@
 import isString from './isString';
 import isFunction from './isFunction';
-import indexloop from './indexloop';
+import indexLoop from './indexLoop';
 
 
 
@@ -10,5 +10,5 @@ import indexloop from './indexloop';
  */
 export default function eachWord(phrase: string, cb?: Function, separator: RegExp|string = /[- _,]+/): number {
   const words = isString(phrase) ? phrase.split(separator) : [];
-  return isFunction(cb) ? indexloop(words, cb) : words.length;
+  return isFunction(cb) ? indexLoop(words, cb) : words.length;
 }
