77ea6a43474366e56cc5e0d6b87fd9ad55323194
Toke Voltelen <toke.voltelen@yoomap.fr>
2018-04-09 17:47:36 +0200

    Improve build

--- tasks/clean.js
+++ tasks/clean.js
@@ -5,7 +5,7 @@ const nPath = require('path');
 glob('./*.ts')
   .then((files) => {
     const fileNames = files.map((file) => nPath.basename(file, '.ts'));
-    return glob([`./@(${fileNames.join('|')}).js`, `./!(node_modules)/@(${fileNames.join('|')}).js?(.map)`]);
+    return glob([`./@(${fileNames.join('|')}).js`, `./!(node_modules)/@(${fileNames.join('|')})?(.spec).js?(.map)`]);
   })
   .then((files) => Promise.all(files.map((file) => fs.remove(file))))
   .then(() => console.log('done'));
