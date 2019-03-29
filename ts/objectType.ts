8b2c31cc1da4b567e16e07c958728f4f66c61b16
Toke Voltelen <tokevoltelen@gmail.com>
2019-03-29 10:39:54 +0100

    objectType: Calirfy code a bit

--- ts/objectType.ts
+++ ts/objectType.ts
@@ -8,8 +8,9 @@
  * ```
  *
  * @param obj - Object to get the type of
- * @return - String representation of the object type
+ * @return - Lower case string representation of the object type
  */
 export default function objectType(obj: any): string {
-  return Object.prototype.toString.call(obj).toLowerCase().match(/(\w+)]$/)[1];
+  const type = ({}).toString.call(obj);
+  return type.substring(8, type.length - 1).toLowerCase();
 }
