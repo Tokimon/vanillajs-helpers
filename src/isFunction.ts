bc2296906a6f2697a2b949539332579d60520474
Toke Voltelen <tokevoltelen@gmail.com>
2021-06-23 15:07:50 -0700

    Create validation.yml (#37)

--- src/isFunction.ts
+++ src/isFunction.ts
@@ -10,6 +10,7 @@
  * @param obj - Object to test
  * @return - Whether the object a Function or not
  */
+// eslint-disable-next-line @typescript-eslint/ban-types
 export default function isFunction(obj: unknown): obj is Function {
   return typeof obj === 'function';
 }
