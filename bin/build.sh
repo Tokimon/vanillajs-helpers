510601a7849516b931c6df2ac57a9bbfcccc0e66
Tokimon <tvo@leapwork.com>
2022-01-04 01:34:24 +0100

    minor updates to bin files

--- bin/build.sh
+++ bin/build.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 print_done() {
   echo -e '\e[1;32mdone\e[0m'
 }
 
 print_building() {
-  printf "Building \e[1;33m.$1\e[0m ... "
+  printf "Building \e[1;33m$1\e[0m ... "
 }
 
 build() {
-  print_building $1
+  print_building .$1
   
   # Compile the typescript files
   npx tsc -p tsconfig.$1.json
@@ -24,14 +24,20 @@ build() {
   print_done
 }
 
-# Preparing the accumulated index file
-rm -f ./src/index.ts
+build_index() {
+  print_building index
 
-for file in ./src/*.ts; do
-  name=$(basename $file .ts)
-  echo "export { default as $name } from './$name';" >> ./src/index.ts
-done
+  rm -f ./src/index.ts
 
+  for file in ./src/*.ts; do
+    name=$(basename $file .ts)
+    echo "export { default as $name } from './$name';" >> ./src/index.ts
+  done
+
+  print_done
+}
+
+build_index
 build mjs
 build cjs
 
